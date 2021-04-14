bee1fd0828feb3e9340519af6ab923860647376a
Seojin Kim <skim27@wustl.edu>
2021-04-13 23:03:40 -0500

    changes static

--- RMD/urls.py
+++ RMD/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('', include('frontend.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
