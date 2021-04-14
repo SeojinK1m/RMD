1b7b0c37e603a73f31816b963938975a942cb2d3
Seojin Kim <skim27@wustl.edu>
2021-04-13 23:09:55 -0500

    changes static

--- RMD/urls.py
+++ RMD/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from rest_framework import routers
 from backend import views
 from django.conf.urls.static import static
+import settings
 
 router = routers.DefaultRouter()
 router.register(r'schools', views.SchoolsViewset)
